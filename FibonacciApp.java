592d2c66f063248e3898a5050b1ef9047951e478
ClementMabila <140471915+ClementMabila@users.noreply.github.com>
2023-07-31 18:52:03 +0200

    Add files via upload
    This is the GUI class for a Fibonacci Number game where user get to guess if the number is Fibonacci or Not Fibonacci ,a random number is generated by the computer

---
+++ FibonacciApp.java
@@ -0,0 +1,14 @@
+
+package fibonacciapp;
+
+import za.ac.tut.fibonicnumbers.FibonicNumbers;
+
+public class FibonacciApp {
+
+    public static void main(String[] args) {
+    
+        FibonicNumbers myApp;
+        myApp = new FibonicNumbers();
+    }
+    
+}
